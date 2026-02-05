701b7b8b2fbf98a48deff4919a2f85a6ec98a230
levanduy093-work <levanduy.work@gmail.com>
2026-02-05 16:05:59 +0700

    Update Homebrew formula for sshm to version 1.0.1 and update SHA256 checksum

--- homebrew-formula/sshm.rb
+++ homebrew-formula/sshm.rb
@@ -1,8 +1,8 @@
 class Sshm < Formula
   desc "Terminal UI for SSH Host Management with auto-discovery and intelligent username detection"
   homepage "https://github.com/levanduy093-work/ssh_management"
-  url "https://github.com/levanduy093-work/ssh_management/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "e9a0af78e737e0eaff9b4eeea802eb9bb8266bcb7f86f42bd3f06553f0b35523"
+  url "https://github.com/levanduy093-work/ssh_management/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   head "https://github.com/levanduy093-work/ssh_management.git", branch: "main"
 
